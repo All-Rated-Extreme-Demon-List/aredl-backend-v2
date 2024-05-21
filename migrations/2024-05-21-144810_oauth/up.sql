78e95622b50d7a8a329188de66910414a2578c85
Simon <simon.giesse@gmx.de>
2024-05-21 18:02:38 +0200

    Include oauth migrations

---
+++ migrations/2024-05-21-144810_oauth/up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE oauth_requests (
+    csrf_state VARCHAR NOT NULL,
+    pkce_verifier VARCHAR NOT NULL,
+    nonce VARCHAR NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(csrf_state)
+);
