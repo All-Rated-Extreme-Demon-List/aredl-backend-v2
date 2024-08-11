b58292487d21cb2fd88e61d7021088ad1384b023
Simon <simon.giesse@gmx.de>
2024-08-11 19:49:35 +0200

    Undoing that lmao

--- src/main.rs
+++ src/main.rs
@@ -49,7 +49,6 @@ async fn main() -> std::io::Result<()> {
             .allow_any_origin()
             .allow_any_method()
             .allow_any_header()
-            .supports_credentials()
             .max_age(3600);
 
         App::new()
