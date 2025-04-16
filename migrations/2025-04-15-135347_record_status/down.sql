2e8097d4815253070c3bab48c7eb0af6e2610a6a
Minebox260 <joellascoumes@gmail.com>
2025-04-16 19:25:03 +0200

    migration sql fix

--- migrations/2025-04-15-135347_record_status/down.sql
+++ migrations/2025-04-15-135347_record_status/down.sql
@@ -1,9 +1,9 @@
 ALTER TABLE aredl_submissions 
-    DROP COLUMN status
+    DROP COLUMN status;
 
 DROP TYPE submission_status;
 
 ALTER TABLE aredl_submissions
-    CREATE COLUMN is_rejected boolean DEFAULT false NOT NULL;
+    ADD COLUMN is_rejected boolean DEFAULT false NOT NULL;
 
 
