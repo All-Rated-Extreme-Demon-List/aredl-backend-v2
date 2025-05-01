5b28f5da2553e26ea9bce14b94733ddb8f660119
Minebox260 <joellascoumes@gmail.com>
2025-05-01 19:21:16 +0200

    forgot to reset submission view

--- migrations/2025-05-01-150648_submission_history_fields/down.sql
+++ migrations/2025-05-01-150648_submission_history_fields/down.sql
@@ -9,3 +9,14 @@ ALTER TABLE submission_history
 
 ALTER TABLE aredl_submissions
 	DROP COLUMN IF EXISTS updated_at;
+
+DROP VIEW IF EXISTS aredl_submissions_with_priority;
+
+CREATE OR REPLACE VIEW aredl_submissions_with_priority AS
+SELECT 
+    *,
+    -- epoch is # of seconds passed since 1970
+    (EXTRACT(EPOCH FROM NOW()) - EXTRACT(EPOCH FROM created_at))::BIGINT + 
+    -- 21600 is # of seconds in 6
+    CASE WHEN priority = TRUE THEN 21600 ELSE 0 END AS priority_value
+FROM aredl_submissions;
--- migrations/2025-05-01-150648_submission_history_fields/up.sql
+++ migrations/2025-05-01-150648_submission_history_fields/up.sql
@@ -9,3 +9,14 @@ ADD COLUMN IF NOT EXISTS reviewer_id UUID;
 
 ALTER TABLE aredl_submissions
 ADD COLUMN IF NOT EXISTS updated_at TIMESTAMP NOT NULL DEFAULT NOW();
+
+DROP VIEW IF EXISTS aredl_submissions_with_priority;
+
+CREATE OR REPLACE VIEW aredl_submissions_with_priority AS
+SELECT 
+    *,
+    -- epoch is # of seconds passed since 1970
+    (EXTRACT(EPOCH FROM NOW()) - EXTRACT(EPOCH FROM created_at))::BIGINT + 
+    -- 21600 is # of seconds in 6
+    CASE WHEN priority = TRUE THEN 21600 ELSE 0 END AS priority_value
+FROM aredl_submissions;
