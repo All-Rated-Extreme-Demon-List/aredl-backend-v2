294f48b62b68f6f11b9a3f8a140a4399983b0125
Minebox260 <joellascoumes@gmail.com>
2026-02-16 23:04:28 +0100

    fix: changing legacy status also triggers points recalculation

---
+++ migrations/2026-02-16-213810_fix_legacy_points_update/up.sql
@@ -0,0 +1,11 @@
+DROP TRIGGER IF EXISTS levels_points_before_update ON aredl.levels;
+CREATE TRIGGER levels_points_before_update
+BEFORE UPDATE OF "position", "legacy" ON aredl.levels
+FOR EACH ROW
+EXECUTE PROCEDURE aredl.levels_points_before_update();
+
+DROP TRIGGER IF EXISTS levels_points_before_update ON arepl.levels;
+CREATE TRIGGER levels_points_before_update
+BEFORE UPDATE OF "position", "legacy" ON arepl.levels
+FOR EACH ROW
+EXECUTE PROCEDURE arepl.levels_points_before_update();
