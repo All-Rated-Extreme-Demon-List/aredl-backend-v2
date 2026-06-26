d4d2fb7e02392a974404751f67771bbb8729ed5c
Minebox260 <joellascoumes@gmail.com>
2026-06-26 21:04:01 +0200

    imp(cache): cache utils

--- src/cache_control.rs
+++ src/cache_control.rs
@@ -1,7 +1,7 @@
 use actix_web::{
     body::MessageBody,
     dev::{Service, ServiceRequest, ServiceResponse, Transform},
-    http::header::{self, CacheControl, CacheDirective, TryIntoHeaderValue},
+    http::header::{self, CacheControl, CacheDirective, HeaderValue, TryIntoHeaderValue as _},
     Error,
 };
 use futures_util::future::{ready, LocalBoxFuture, Ready};
@@ -12,28 +12,56 @@ use std::{
 };
 
 pub struct CacheController {
-    cache_directive: Vec<CacheDirective>,
+    cache_mode: CacheMode,
     replace: bool,
+}
+
+enum CacheMode {
+    Static(Vec<CacheDirective>),
+    AuthPublicMaxAge(u32),
+}
+
+fn cache_header_value(directives: Vec<CacheDirective>) -> HeaderValue {
+    match CacheControl(directives).try_into_value() {
+        Ok(value) => value,
+        Err(error) => {
+            tracing::error!("Failed to build cache-control header: {error}");
+            HeaderValue::from_static("no-store")
+        }
+    }
 }
 
 impl CacheController {
     pub fn default_no_store() -> Self {
         Self {
-            cache_directive: vec![CacheDirective::NoCache, CacheDirective::NoStore],
+            cache_mode: CacheMode::Static(vec![CacheDirective::NoCache, CacheDirective::NoStore]),
             replace: false,
         }
     }
 
     pub fn public_with_max_age(seconds: u32) -> Self {
         Self {
-            cache_directive: vec![CacheDirective::Public, CacheDirective::MaxAge(seconds)],
+            cache_mode: CacheMode::Static(vec![
+                CacheDirective::Public,
+                CacheDirective::MaxAge(seconds),
+            ]),
             replace: true,
         }
     }
 
     pub fn private_with_max_age(seconds: u32) -> Self {
         Self {
-            cache_directive: vec![CacheDirective::Private, CacheDirective::MaxAge(seconds)],
+            cache_mode: CacheMode::Static(vec![
+                CacheDirective::Private,
+                CacheDirective::MaxAge(seconds),
+            ]),
+            replace: true,
+        }
+    }
+
+    pub fn auth_public_with_max_age(seconds: u32) -> Self {
+        Self {
+            cache_mode: CacheMode::AuthPublicMaxAge(seconds),
             replace: true,
         }
     }
@@ -51,22 +79,42 @@ where
     type Future = Ready<Result<Self::Transform, Self::InitError>>;
 
     fn new_transform(&self, service: S) -> Self::Future {
-        let header_value = CacheControl(self.cache_directive.clone())
-            .try_into_value()
-            .expect("Invalid cache directive");
+        let cache_mode = match &self.cache_mode {
+            CacheMode::Static(directives) => {
+                CacheModeRuntime::Static(cache_header_value(directives.clone()))
+            }
+            CacheMode::AuthPublicMaxAge(seconds) => CacheModeRuntime::AuthPublicMaxAge {
+                public_header_value: cache_header_value(vec![
+                    CacheDirective::Public,
+                    CacheDirective::MaxAge(*seconds),
+                ]),
+                private_header_value: cache_header_value(vec![
+                    CacheDirective::Private,
+                    CacheDirective::MaxAge(*seconds),
+                ]),
+            },
+        };
 
         ready(Ok(CacheControlMiddleware::<S, B> {
             service: Rc::new(service),
-            header_value,
+            cache_mode,
             replace: self.replace,
             _marker: PhantomData,
         }))
     }
 }
 
+enum CacheModeRuntime {
+    Static(HeaderValue),
+    AuthPublicMaxAge {
+        public_header_value: HeaderValue,
+        private_header_value: HeaderValue,
+    },
+}
+
 pub struct CacheControlMiddleware<S, B> {
     service: Rc<S>,
-    header_value: header::HeaderValue,
+    cache_mode: CacheModeRuntime,
     replace: bool,
     _marker: PhantomData<B>,
 }
@@ -85,15 +133,34 @@ where
     }
 
     fn call(&self, req: ServiceRequest) -> Self::Future {
+        let has_authorization = req.headers().contains_key(header::AUTHORIZATION);
         let fut = self.service.call(req);
         let replace = self.replace;
-        let header_value = self.header_value.clone();
+        let header_value = match &self.cache_mode {
+            CacheModeRuntime::Static(header_value) => header_value.clone(),
+            CacheModeRuntime::AuthPublicMaxAge {
+                public_header_value,
+                private_header_value,
+            } => {
+                if has_authorization {
+                    private_header_value.clone()
+                } else {
+                    public_header_value.clone()
+                }
+            }
+        };
+        let vary_on_authorization =
+            matches!(self.cache_mode, CacheModeRuntime::AuthPublicMaxAge { .. });
 
         Box::pin(async move {
             let mut res = fut.await?;
             if !res.headers().contains_key(header::CACHE_CONTROL) || replace {
                 res.headers_mut()
                     .insert(header::CACHE_CONTROL, header_value);
+            }
+            if vary_on_authorization {
+                res.headers_mut()
+                    .insert(header::VARY, HeaderValue::from_static("Authorization"));
             }
             Ok(res)
         })
