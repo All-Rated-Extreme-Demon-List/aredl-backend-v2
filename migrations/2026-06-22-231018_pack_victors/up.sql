5e4ed9ae6d40b80e56bdf8c8b5a6ee25c607eb81
Minebox260 <joellascoumes@gmail.com>
2026-07-09 20:33:18 +0200

    fix(packs): exclude banned/redacted from pack completions

--- migrations/2026-06-22-231018_pack_victors/up.sql
+++ migrations/2026-06-22-231018_pack_victors/up.sql
@@ -9,6 +9,7 @@ CREATE OR REPLACE VIEW aredl.completed_packs AS
         pl.pack_id,
         MAX(r.achieved_at) AS completed_at
     FROM aredl.records r
+    JOIN users u ON u.id = r.submitted_by AND u.ban_level <= 1
     JOIN aredl.pack_levels pl ON pl.level_id = r.level_id
     JOIN pcl ON pcl.pack_id = pl.pack_id
     GROUP BY r.submitted_by, pl.pack_id, pcl.lc
@@ -25,6 +26,7 @@ CREATE OR REPLACE VIEW arepl.completed_packs AS
         pl.pack_id,
         MAX(r.achieved_at) AS completed_at
     FROM arepl.records r
+    JOIN users u ON u.id = r.submitted_by AND u.ban_level <= 1
     JOIN arepl.pack_levels pl ON pl.level_id = r.level_id
     JOIN pcl ON pcl.pack_id = pl.pack_id
     GROUP BY r.submitted_by, pl.pack_id, pcl.lc
